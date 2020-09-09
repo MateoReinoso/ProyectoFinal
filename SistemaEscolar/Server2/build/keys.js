95eb1210ea4e4dbc3830c2cb51043c773ff0b8f4
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-09-09 11:08:09 -0500

    restnew

---
+++ SistemaEscolar/Server2/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'br1he3ydbvwczq2gziyd-mysql.services.clever-cloud.com',
+        user: 'ujjyf3gvlocovukq',
+        password: '9QpZlzvcXJzhAVLVB0Zx',
+        database: 'br1he3ydbvwczq2gziyd'
+    }
+};
---
+++ SistemaEscolar/Server2/src/keys.ts
@@ -0,0 +1,8 @@
+export default {
+    database: {
+        host: 'br1he3ydbvwczq2gziyd-mysql.services.clever-cloud.com',
+        user: 'ujjyf3gvlocovukq',
+        password: '9QpZlzvcXJzhAVLVB0Zx',
+        database: 'br1he3ydbvwczq2gziyd'
+    }
+}
