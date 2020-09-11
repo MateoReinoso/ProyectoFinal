acd814a23020704d305a1dc34905c612cc3b9f61
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-09-11 14:45:08 -0500

    muestra datos

--- SistemaEscolar/client/src/app/intranet/student/home/home.component.ts
+++ SistemaEscolar/client/src/app/intranet/student/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../../../services/login.service'
+import { LoginService } from '../../../services/login.service';
+import { SchoolService } from '../../../services/school.service';
+import { Login  } from '../../../models/School';
 
 @Component({
   selector: 'app-home-student',
@@ -10,16 +12,25 @@ export class HomeComponentStudent implements OnInit {
 
 
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private schoolService: SchoolService) { }
 
-  credential: any =[];
+  credential: any = [];
   
   ngOnInit(): void {
-    this.credential=this.loginService.getsession();
+    this.credential = this.loginService.getsession();
     console.log(this.loginService.getsession());
-    console.log(this.credential);  
+    console.log(this.credential);
   }
 
-  
+  getNotes() {
+    this.schoolService.getNotes(this.credential.COD_PERSONA)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.credential = res;
+        },
+        err => console.error(err)
+      );
+  }
 
 }
