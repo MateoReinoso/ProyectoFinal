95bfcab715dbe307e06884739528ed366e6be7d6
jmherediaespe <jmheredia@espe.edu.ec>
2020-09-14 22:58:01 -0500

    Update keys.js

--- SistemaEscolar/Server2/build/keys.js
+++ SistemaEscolar/Server2/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'br1he3ydbvwczq2gziyd-mysql.services.clever-cloud.com',
-        user: 'ujjyf3gvlocovukq',
-        password: '9QpZlzvcXJzhAVLVB0Zx',
-        database: 'br1he3ydbvwczq2gziyd'
+        host: 'us-cdbr-east-02.cleardb.com',
+        user: 'b4067b4009d59f',
+        password: '30bd2c09',
+        database: 'heroku_79b58406d81ead2'
     }
 };
